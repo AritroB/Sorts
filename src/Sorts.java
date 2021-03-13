d129f609fa27b0d9b1b8af8287ef5e95406037c8
AritroB <abasuusa2010@gmail.com>
2021-03-13 10:36:59 -0600

    Added BubbleSortImproved Method

--- src/Sorts.java
+++ src/Sorts.java
@@ -10,4 +10,20 @@ public class Sorts {
             }
         }
     }
+
+    public void BubbleSortImproved(int[] a) {
+        boolean swapped = true;
+        for (int pass = a.length - 1; pass >= 0 && swapped; pass--) {
+            swapped = false;
+            for ( int i = 0; i <= pass - 1; i++) {
+                if (a[i] > a[i+1]) {
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+
+    }
 }
