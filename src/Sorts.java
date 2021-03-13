b9713b8a2e1332bf23a19728814b9dbf08999e70
AritroB <abasuusa2010@gmail.com>
2021-03-13 10:44:24 -0600

    added Selection Sort method and runner class

--- src/Sorts.java
+++ src/Sorts.java
@@ -24,6 +24,19 @@ public class Sorts {
                 }
             }
         }
+    }
 
+    public void Selection(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
     }
 }
